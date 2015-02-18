e3024eea334425dd103e03c34a6241ad5e523a3c
h-naoto <hanaue.naoto@po.ntts.co.jp>
2015-02-19 02:42:30 +0900

    add docker control routine and option parser

--- control.py
+++ control.py
@@ -1,18 +1,19 @@
 import os
 from fabric.api import local
 
-BRIDGE = {"name": "br0", "addr": "192.168.10.254", "mask": "/24"}
-POS_ADDR1 = "192.168.10.1"
-POS_ADDR2 = "192.168.10.2"
-SHEEP_ADDR1 = "192.168.10.11"
-SHEEP_ADDR2 = "192.168.10.12"
-SHEEP_ADDR3 = "192.168.10.13"
-POS_ADDRS = [POS_ADDR1, POS_ADDR2]
-SHEEP_ADDS = [SHEEP_ADDR1, SHEEP_ADDR2, SHEEP_ADDR3]
+BRIDGE = {"name": "br0", "addr": "10.0.10.254", "mask": "/24"}
+POS1 = {"name": "postgres1", "addr": "10.0.10.1", "image": "nhanaue/postgres"}
+POS2 = {"name": "postgres2", "addr": "10.0.10.2", "image": "nhanaue/postgres"}
+SHEEP1 = {"name": "sheepdog1", "addr": "10.0.10.11", "image": "nhanaue/sheepdog"}
+SHEEP2 = {"name": "sheepdog2", "addr": "10.0.10.12", "image": "nhanaue/sheepdog"}
+SHEEP3 = {"name": "sheepdog3", "addr": "10.0.10.13", "image": "nhanaue/sheepdog"}
+POS = [POS1, POS2]
+SHEEP = [SHEEP1, SHEEP2, SHEEP3]
 
 SHARE_DIR = "/tmp/posdog"
 
-def test_user_check():
+
+def check_user():
     root = False
     outbuf = local("echo $USER", capture=True)
     user = outbuf
@@ -24,8 +25,8 @@ def test_user_check():
 
 def install_docker_and_tools():
     print "start install packages of test environment."
-    if test_user_check() is False:
-        print "you are not root"
+    if check_user() is False:
+        print "Please running in root."
         return
 
     local("apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "
@@ -38,7 +39,6 @@ def install_docker_and_tools():
     local("gpasswd -a `whoami` docker", capture=True)
     local("apt-get install -y --force-yes iputils-arping", capture=True)
     local("apt-get install -y --force-yes bridge-utils", capture=True)
-    local("apt-get install -y --force-yes tcpdump", capture=True)
     local("apt-get install -y --force-yes lv", capture=True)
     local("wget https://raw.github.com/jpetazzo/pipework/master/pipework -O /usr/local/bin/pipework",
           capture=True)
@@ -48,7 +48,7 @@ def install_docker_and_tools():
     local("docker pull nhanaue/postgres", capture=True)
 
 
-def bridge_setting_check():
+def check_bridge():
     setting_exists = False
     sysfs_name = "/sys/class/net/" + BRIDGE["name"]
     if os.path.exists(sysfs_name):
@@ -57,13 +57,103 @@ def bridge_setting_check():
     return setting_exists
 
 
-def docker_container_set_ipaddr(name, addr):
+def create_bridge():
+    cmd = "brctl addbr " + BRIDGE["name"]
+    local(cmd, capture=True)
+    cmd = "ifconfig " + BRIDGE["name"] + " " + BRIDGE["addr"]
+    local(cmd, capture=True)
+    cmd = "ifconfig " + BRIDGE["name"] + " up"
+    local(cmd, capture=True)
+
+
+def destroy_bridge():
+    sysfs_name = "/sys/class/net/" + BRIDGE["name"]
+    if os.path.exists(sysfs_name):
+        cmd = "ifconfig " + BRIDGE["name"] + " down"
+        local(cmd, capture=True)
+        cmd = "brctl delbr " + BRIDGE["name"]
+        local(cmd, capture=True)
+
+
+def set_ipaddr(name, addr):
+    addr += "/24"
     cmd = "pipework %s %s %s" % (BRIDGE["name"], name, addr)
     local(cmd, capture=True)
 
 
-def docker_container_run(name, addr):
-    image_name = "nhanaue/%s" % name
-    cmd = "docker run --privileged=true -v %s:/root/share_volume --name %s -id %s" % (SHARE_DIR, name, image_name)
+def check_container():
+    container_exists = False
+    outbuf = local("docker ps -a", capture=True)
+    docker_ps = outbuf.split('\n')
+    for container in docker_ps:
+        container_name = container.split()[-1]
+        for pos in POS:
+            if pos["name"] == container_name:
+                container_exists = True
+        for sheep in SHEEP:
+            if sheep["name"] == container_name:
+                container_exists = True
+    return container_exists
+
+
+def get_container():
+    containers = []
+    cmd = "docker ps -a | awk '{print $NF}'"
+    outbuf = local(cmd, capture=True)
+    docker_ps = outbuf.split('\n')
+    for container in docker_ps:
+        if container != "NAMES":
+            containers.append(container.split()[-1])
+    return containers
+
+
+def run_container(instans):
+    image_name = "nhanaue/%s" % instans["name"]
+    # cmd = "docker run --privileged=true -v %s:/root/share_volume --name %s -id %s" % (SHARE_DIR, name, image_name)
+    cmd = "docker run --privileged=true --name %s -id %s" % (instans["name"], instans["image"])
     local(cmd, capture=True)
-    docker_container_set_ipaddr(name, addr)
+    set_ipaddr(instans["name"], instans["addr"])
+    
+
+def stop_container(name):
+    cmd = "docker rm -f " + name
+    local(cmd, capture=True)
+
+
+def check_posdog_environment():
+    env_exists = False
+    if check_bridge() or check_container():
+        env_exists = True
+    return env_exists
+
+
+def create_posdog_environment():
+
+    # initial environment for postgres and sheepdog
+    if check_posdog_environment():
+        return False
+
+    # create bridge
+    create_bridge()
+    # run sheepdog on docker container
+    for sheepdog in SHEEP:
+        run_container(sheepdog)
+    # run postgres on docker container
+    postgres = POS[0]
+    run_container(postgres)
+
+    return True
+
+
+def destroy_posdog_environment():
+    containers = get_container()
+    for container in containers:
+        container_name = container.split()[-1]
+        for pos in POS:
+            if pos["name"] == container_name:
+                stop_container(container_name)
+        for sheep in SHEEP:
+            if sheep["name"] == container_name:
+                stop_container(container_name)
+    destroy_bridge()
+
---
+++ posdog.py
@@ -0,0 +1,42 @@
+import control as cl
+from optparse import OptionParser, OptionValueError
+
+
+def op_parse():
+    usage = "usage: %prog [options] keyword"
+    parser = OptionParser(usage)
+    parser.add_option(
+        "-t", "--task",
+        type="choice",
+        choices=["install", "create", "destroy"],
+        default="create",
+        dest="task",
+        help="meaningless option"
+    )
+
+    return parser
+
+
+def main():
+    if cl.check_user() is False:
+        print "Please running in root."
+        return
+
+    parser = op_parse()
+    (options, args) = parser.parse_args()
+
+    task = options.task
+    if task == "install":
+        cl.install_docker_and_tools()
+    elif task == "create":
+        res = cl.create_posdog_environment()
+        if res is False:
+            print "container or bridge configuration is present."
+            print "execute [python posdog.py -t destroy] command."
+    elif task == "destroy":
+        cl.destroy_posdog_environment()
+    else:
+        print "invalid option"
+
+if __name__ == "__main__":
+    main()
